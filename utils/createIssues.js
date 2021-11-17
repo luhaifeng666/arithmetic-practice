3b16f92c9f1bf727c0837df3726edac34b66477e
luhaifeng <lhf222458@ncarzone.com>
2021-11-17 14:57:22 +0800

    feat: 自动设置issue标题

--- utils/createIssues.js
+++ utils/createIssues.js
@@ -1,11 +1,34 @@
 const { Octokit } = require("octokit");
 const core = require('@actions/core');
+const docs = require('./getAllDocs')
 
 const auth = core.getInput('token')
 const octokit = new Octokit({ auth })
 
-octokit.rest.issues.create({
-	owner: "luhaifeng666",
-	repo: "arithmetic-practice",
-	title: "Hello world from",
-});
+// 定义项目通用参数
+const REPO_INFO = {
+	owner: "octocat",
+	repo: "hello-world"
+}
+// 获取所有issues
+let issuesList = []
+const allIssues = octokit.paginate.iterator(octokit.rest.issues.listForRepo, {
+	...REPO_INFO,
+	per_page: 100,
+})
+
+for await (const { data: issues } of allIssues) {
+	issuesList = issues.map(issue => issue.name)
+}
+
+// 获取没有创建issue的标题
+const titles = docs.filter(title => !issuesList.includes(title))
+
+// 将没有创建issue的算法都创建对应的issue
+titles.forEach(title => {
+	octokit.rest.issues.create({
+		...REPO_INFO,
+		title,
+		body: `关于${title}的更多解法，欢迎在issue中讨论~`
+	});
+})
---
+++ utils/getAllDocs.js
@@ -0,0 +1,23 @@
+const vuepressConfig = require("../docs/.vuepress/config");
+const fs = require("fs");
+const path = require("path");
+const {parse} = require("markdown-to-ast");
+
+const baseDir = '/notes/exercises/'
+const { themeConfig: { sidebar } } = vuepressConfig
+const exercisesFiles = sidebar[`${baseDir}`].reduce((files, item) => {
+	if ( item && item.children ) {
+		files.push(...item.children)
+	}
+	return files
+}, [])
+module.exports = exercisesFiles.map(fileDir => {
+	const content = fs.readFileSync(path.resolve(__dirname, `docs${baseDir}${fileDir}`), {
+		encoding: 'utf-8'
+	})
+	const { children } = parse(content)
+	const titleConfig = children.filter(ch => ch.type === 'Header' && ch.depth === 1)
+	let title = ''
+	if (titleConfig && titleConfig.length) title = titleConfig[0].raw.replace('# ', '')
+	return title
+})
