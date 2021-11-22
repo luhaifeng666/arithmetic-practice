b4406beebdc76ecfbffa589f0133d4ac91293e5e
luhaifeng <lhf222458@ncarzone.com>
2021-11-22 09:32:11 +0800

    feat: 修改READM内容

--- constants/common.js
+++ constants/common.js
@@ -11,9 +11,11 @@ const README_CONFIG = [
 ![octokit](https://img.shields.io/badge/octokit-%5E1.7.0-lightgrey)
 ![typescript](https://img.shields.io/badge/typescript-%5E4.4.2-blue)
 ![vuepress](https://img.shields.io/badge/vuepress-%5E1.8.2-brightgreen)
-![coverage](https://img.shields.io/badge/coverage-96.61%25-green)
+![coverage](https://img.shields.io/badge/coverage-97.4%25-green)
 
 算法练习笔记, 从零开始，（争取）每日一题！冲！欢迎⭐️！
+
+有兴趣的小伙伴可以在issue里留下自己的骚操作，一起学习探讨~
 
 项目地址[请戳](https://luhaifeng666.github.io/arithmetic-practice/)~
 
